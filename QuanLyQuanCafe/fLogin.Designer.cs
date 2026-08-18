7e5a672e6642b2982c94504985fe0816bd136885
Mark <hoanglinh171038@gmail.com>
2026-08-18 08:36:31 +0700

    Add main form and table layout, exit confirmation on login

--- QuanLyQuanCafe/fLogin.Designer.cs
+++ QuanLyQuanCafe/fLogin.Designer.cs
@@ -29,23 +29,113 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.tbxUsername = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.tbxPassword = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(71, 36);
+            this.panel1.Controls.Add(this.tbxUsername);
+            this.panel1.Controls.Add(this.lblUsername);
+            this.panel1.Location = new System.Drawing.Point(62, 28);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(656, 141);
+            this.panel1.Size = new System.Drawing.Size(552, 47);
             this.panel1.TabIndex = 0;
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Location = new System.Drawing.Point(15, 13);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(59, 16);
+            this.lblUsername.TabIndex = 0;
+            this.lblUsername.Text = "Username";
+            this.lblUsername.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // tbxUsername
+            // 
+            this.tbxUsername.Location = new System.Drawing.Point(81, 11);
+            this.tbxUsername.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tbxUsername.Name = "tbxUsername";
+            this.tbxUsername.Size = new System.Drawing.Size(458, 22);
+            this.tbxUsername.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.tbxPassword);
+            this.panel2.Controls.Add(this.lblPassword);
+            this.panel2.Location = new System.Drawing.Point(62, 91);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(552, 47);
+            this.panel2.TabIndex = 1;
+            // 
+            // tbxPassword
+            // 
+            this.tbxPassword.Location = new System.Drawing.Point(81, 11);
+            this.tbxPassword.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tbxPassword.Name = "tbxPassword";
+            this.tbxPassword.Size = new System.Drawing.Size(458, 22);
+            this.tbxPassword.TabIndex = 2;
+            this.tbxPassword.UseSystemPasswordChar = true;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(3, 13);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(58, 16);
+            this.lblPassword.TabIndex = 0;
+            this.lblPassword.Text = "Password";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Font = new System.Drawing.Font("Arial Narrow", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.Location = new System.Drawing.Point(539, 157);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(62, 27);
+            this.btnLogin.TabIndex = 3;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(474, 158);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(52, 27);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
             // fLogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(700, 450);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Arial Narrow", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "fLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.fLogin_FormClosing);
+            this.Load += new System.EventHandler(this.fLogin_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -53,6 +143,13 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.TextBox tbxUsername;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox tbxPassword;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnExit;
     }
 }
 
--- QuanLyQuanCafe/fLogin.cs
+++ QuanLyQuanCafe/fLogin.cs
@@ -16,5 +16,40 @@ namespace QuanLyQuanCafe
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        // THIS WAY IS OK BUT THE KTEAM WAY IS BETTER: PUT THE RULE WHERE ALL PATH PASS THROUGH, NOT ON ONE ENTRY POINT.
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            //DialogResult result =  MessageBox.Show("Do you want to exit the appkication ?", "Notification", MessageBoxButtons.OKCancel);
+            // if (result ==  DialogResult.OK)
+            // {
+            //     Application.Exit();
+            // }
+            Application.Exit();
+        }
+
+        private void fLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fLogin_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Do you want to exit the application ?", "Notification", MessageBoxButtons.OKCancel) != System.Windows.Forms.DialogResult.OK)
+            {
+                e.Cancel = true; // Cancel the closing event if the user does not confirm
+            }
+        }
     }
 }
